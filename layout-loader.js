96946c03ede74687cc31773d6ef211e4abe23b28
Avishikta-admin <avishiktaph1@gmail.com>
2025-07-31 20:14:29 +0530

    Add files via upload

---
+++ layout-loader.js
@@ -0,0 +1,9 @@
+// layout-loader.js
+(function () {
+  const version = '20250731-1'; // Change this when you update layout.js
+
+  const script = document.createElement('script');
+  script.src = `layout.js?v=${version}`;
+  script.defer = true; // So it loads after the HTML is parsed
+  document.head.appendChild(script);
+})();
