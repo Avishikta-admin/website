0f6994e8689eb2cbf2e2059463ee86e9b551fd92
Avishikta-admin <avishiktaph1@gmail.com>
2025-11-20 02:32:40 +0530

    Update layout-loader.js

--- layout-loader.js
+++ layout-loader.js
@@ -1,11 +1,12 @@
 // layout-loader.js
 (function () {
-  const version = '20251105-1'; // Change this when you update layout.js
+  const version = '20251120-2'; // Change this when you update layout.js
 
   const script = document.createElement('script');
   script.src = `layout.js?v=${version}`;
   script.defer = true; // So it loads after the HTML is parsed
   document.head.appendChild(script);
 })();
+
 
 
